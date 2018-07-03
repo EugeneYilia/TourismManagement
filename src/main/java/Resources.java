925a94d0024a5560e945ead511b7c70424076d74
EugeneYilia <liuyichen@eugeneliu.xyz>
2018-07-03 19:53:04 +0800

    finish show spots information

--- src/main/java/Resources.java
+++ src/main/java/Resources.java
@@ -11,6 +11,7 @@ public class Resources {
     private static ArrayList<String> places = new ArrayList<String>();
     private static ArrayList<Spot> spots = new ArrayList<Spot>();
     private static String fileName = "document/spots.txt";
+    private static int[][] distances;
 
     static {
         try {
@@ -29,6 +30,7 @@ public class Resources {
                     places.add(secondPlace);
                 }
             }
+            distances = new int[places.size()][places.size()];
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -55,12 +57,67 @@ public class Resources {
         }
     }
 
+    public static void printResult() {
+        System.out.print("          ");
+        for (String place: places) {
+            System.out.print(place + "        ");
+        }
+        System.out.println();
+        for (int i = 0; i < places.size(); i++) {
+            for (int j = 0; j < places.size(); j++) {
+                distances[i][j] = getTwoPlaceDistanc(places.get(i), places.get(j));
+            }
+        }
+        for (int i = 0; i < places.size(); i++) {
+            if (places.get(i).length() == 2) {
+                System.out.print(places.get(i) + "      ");
+            } else {
+                System.out.print(places.get(i) + "    ");
+            }
+            for (int j = 0; j < places.size(); j++) {
+                if ((distances[i][j] / 10) == 0) {
+                    System.out.print(distances[i][j] + "    " + "        ");
+                } else if ((distances[i][j] / 100) == 0) {
+                    System.out.print(distances[i][j] + "   " + "        ");
+                } else if ((distances[i][j] / 1000) == 0) {
+                    System.out.print(distances[i][j] + "  " + "        ");
+                } else if ((distances[i][j] / 10000) == 0) {
+                    System.out.print(distances[i][j] + " " + "        ");
+                } else {
+                    System.out.print(distances[i][j] + "        ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printResult();
+    }
+
+    private static int getTwoPlaceDistanc(String sourcePlace, String destinationPlace) {
+        if (sourcePlace.equals(destinationPlace)) {
+            return 0;
+        }
+        for (Spot spot: spots) {
+            if (spot.getSourcePlace().equals(sourcePlace) && spot.getDestinationPlace().equals(destinationPlace)) {
+                return spot.getDistance();
+            } else if (spot.getDestinationPlace().equals(sourcePlace) && spot.getSourcePlace().equals(destinationPlace)) {
+                return spot.getDistance();
+            }
+        }
+        return 32767;
+    }
+
     @Test
-    public void testResult() {
+    public void testSpots() {
         System.out.println("#############show spots#############");
         showSpots();
         System.out.println("$$$$$$$$$$$$$show spots$$$$$$$$$$$$$");
+    }
 
+    @Test
+    public void testPlaces() {
         System.out.println("#############show places#############");
         showPlaces();
         System.out.println("$$$$$$$$$$$$$show places$$$$$$$$$$$$$");
